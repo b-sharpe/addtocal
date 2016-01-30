4a3f401a601af7f151dbd00c31b741b1ad903e51
nrackleff <nancy.rackleff@thinkshout.com>
2016-01-30 12:11:11 -0800

    Drupal 8 port WIP

--- src/Plugin/Field/FieldFormatter/AddtocalView.php
+++ src/Plugin/Field/FieldFormatter/AddtocalView.php
@@ -7,23 +7,107 @@ namespace Drupal\addtocal\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FormatterBase;
 
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+
+
+
+
 /**
  * @FieldFormatter(
  *  id = "addtocal_view",
  *  label = @Translation("Add to Cal"),
- *  field_types = {"date&quot;, &quot;datestamp&quot;, &quot;datetime"}
+ *  field_types = {
+ *    "date",
+ *    "datestamp",
+ *    "datetime",
+ *  }
  * )
  */
 class AddtocalView extends FormatterBase {
 
   /**
-   * @FIXME
-   * Move all logic relating to the addtocal_view formatter into this
-   * class. For more information, see:
-   *
-   * https://www.drupal.org/node/1805846
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterInterface.php/interface/FormatterInterface/8
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterBase.php/class/FormatterBase/8
+   * {@inheritdoc}
    */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+
+    $bundle_name = $field['bundles'][$instance['entity_type']][0];
+
+    $display = $instance['display'][$view_mode];
+    $settings = $display['settings'];
+
+    //$field_list = field_info_instances($instance['entity_type'], $bundle_name);
+    $field_list = \Drupal::entityTypeManager()->getDefinitions('node', 'article');
+
+    $description_options = $location_options = array('-1' => 'None');
+
+    $location_field_types = array(
+      'text_textfield',
+      'addressfield_default',
+      'addressfield_standard',
+    );
+
+    foreach ($field_list as $field) {
+      if (in_array($field->widget->type, $location_field_types)) {
+        $location_options[$field['field_name']] = $field['label'];
+      }
+
+      if ($field['widget']['type'] == 'text_textarea' || $field['widget']['type'] == 'text_textarea_with_summary') {
+        $description_options[$field['field_name']] = $field['label'];
+      }
+    }
+
+    $options = array(
+      ''  => t('- None -'),
+      '.' => t('Decimal point'),
+      ',' => t('Comma'),
+      ' ' => t('Space'),
+      chr(8201) => t('Thin space'),
+      "'" => t('Apostrophe'),
+    );
+
+    $elements['thousand_separator'] = array(
+      '#type' => 'select',
+      '#title' => t('Thousand marker'),
+      '#options' => $options,
+      '#default_value' => $this->getSetting('thousand_separator'),
+      '#weight' => 0,
+    );
+
+    $elements['past_events'] = array(
+      '#title' => t('Show Add to Cal widget for Past Events'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('prefix_suffix'),
+      '#weight' => 2,
+    );
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = array();
+    $settings = $this->getFieldSettings();
+
+    foreach ($items as $delta => $item) {
+      $output = $this->numberFormat($item->value);
+
+      // Account for prefix and suffix.
+      if ($this->getSetting('prefix_suffix')) {
+        $prefixes = isset($settings['prefix']) ? array_map('field_filter_xss', explode('|', $settings['prefix'])) : array('');
+        $suffixes = isset($settings['suffix']) ? array_map('field_filter_xss', explode('|', $settings['suffix'])) : array('');
+        $prefix = (count($prefixes) > 1) ? format_plural($item->value, $prefixes[0], $prefixes[1]) : $prefixes[0];
+        $suffix = (count($suffixes) > 1) ? format_plural($item->value, $suffixes[0], $suffixes[1]) : $suffixes[0];
+        $output = $prefix . $output . $suffix;
+      }
+
+      $elements[$delta] = array('#markup' => $output);
+    }
+
+    return $elements;
+  }
 
 }
